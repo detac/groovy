b08d1484871a1f4730fc0fe041605a4c4eb77f27
detac <deta.crealogix@gmail.com>
2017-07-06 11:39:36 +0200

    Update GitPullAll.groovy
    fetch from remotes

--- Git/src/git/pull/GitPullAll.groovy
+++ Git/src/git/pull/GitPullAll.groovy
@@ -6,7 +6,11 @@ println ''
 println '*** GIT PULL START *** '
 println ''
 
-def properties = new JsonSlurper().parse(new File('properties.json'))
+def properties = new JsonSlurper().parse(new File('pullAll.json'))
+
+def results = [:]
+
+def encoding = 'UTF-8'
 
 properties.repositories.each { repository ->
     println ''
@@ -14,26 +18,48 @@ properties.repositories.each { repository ->
     println ''
 
     def cd = 'cmd /c cd ' + repository.location
+	
+	println '> git clean'
+	def clean = cd + ' && git clean -fd'
+	clean.execute().waitForProcessOutput(System.out, System.err)
+	println ''		
 
-    println '> git fetch'
-    def fetch = cd + ' && git fetch'
-    fetch.execute().waitForProcessOutput(System.out, System.err)
-    println ''
+    repository.remotes.each { remote ->
+        println ''
+        println '*** Remote : ' + remote + ' START *** '
+        println ''
+
+        println '> git fetch ' + remote
+        def fetch = cd + ' && git fetch ' + remote
+        fetch.execute().waitForProcessOutput(System.out, System.err)
+        
+        println ''
+        println '*** Remote : ' + remote + ' END *** '
+    }
 
     repository.branches.each { branch ->
 
         println ''
         println '*** Branch : ' + branch + ' START *** '
         println ''
-
+		
         println '> git checkout ' + branch
         def checkout_branch = cd + ' && git checkout ' + branch
         checkout_branch.execute().waitForProcessOutput(System.out, System.err)
-        println ''
-
+        println ''	
+		
         println '> git pull'
         def pull = cd + ' && git pull'
         pull.execute().waitForProcessOutput(System.out, System.err)
+
+        def status = cd + ' && git status'
+        def result = ''
+
+        def output = new ByteArrayOutputStream()
+        status.execute().waitForProcessOutput(output, output)
+
+        def key = repository.name + ' | ' + branch
+        results[key] = new String(output.toByteArray(), encoding);
 
         println ''
         println '*** Branch : ' + branch + ' END *** '
@@ -55,3 +81,9 @@ properties.repositories.each { repository ->
 println ''
 println '*** GIT PULL END ***'
 println ''
+
+println '*** RESULTS ***'
+for (it in results) {
+    println it.key
+    println it.value
+}
